18d7e01968ea8623246dae8e7dc870873e48f8fb
kalpana-99 <kalpana.ithagoni@wavelabs.ai>
2021-09-02 18:03:59 +0530

    Modified App.java

--- src/main/java/com/google/drive/App.java
+++ src/main/java/com/google/drive/App.java
@@ -11,5 +11,6 @@ public class App
 	public static void main(String[] args)
     {
         System.out.println(WordUtils.capitalize("hello world!"));
+System.out.println(WordUtils.capitalize("kalpana"));
     }
 }
